df51c81facb741c2c7847070e6703e57e877e77d
Heike <leitte@cs.uni-kl.de>
2024-04-02 19:02:31 +0200

    add copyright text

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,12 +1,12 @@
 <script>
     import '$lib/css/theme.css'
-    export let copyright = 'Copyright';
+    export let copyright = 'Created by <a href="https://leitte.github.io/color-palette/">Heike Leitte</a> using <a href="https://svelte.dev">svelte</a> and <a href="https://gka.github.io/chroma.js/">chroma-js</a>. Source code available on <a href="https://github.com/leitte/color-palette">Github</a>.';
 </script>
 
 <footer class="site-footer">
     <div class="wrapper">
         <div class="site-footer-inner">
-            <div>{copyright}</div>
+            <div>{@html copyright}</div>
         </div>
     </div>
 </footer>
